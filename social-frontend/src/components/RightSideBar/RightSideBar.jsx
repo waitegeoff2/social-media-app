b94488acbe4944fd967e86dc9804023403774e66
Geoffrey Waite <186988887+waitegeoff2@users.noreply.github.com>
2025-11-13 16:55:17 -0500

    Added styling Changed friend index for scrollable boxes Reformatted right sidebar on profile

--- social-frontend/src/components/RightSideBar/RightSideBar.jsx
+++ social-frontend/src/components/RightSideBar/RightSideBar.jsx
@@ -5,9 +5,13 @@ export default function RightSideBar() {
     return (
         <>
         <div className='right-sidebar'>
-            <h3 className='rightside-title'>Welcome to frogbook!</h3>
-            <br />
-            <span>frogbooks replicates the nostalgic look and feel of Facebook's old-school design. <br></br> I built this to show off frontend and backend development skills, including Node.js, Express, React, and more!</span>
+            <div className="frogbook-intro-text">
+                <h3 className='rightside-title'>Welcome to frogbook!</h3>
+                <div className="frogbook-small-text">
+                    <span>frogbooks replicates the nostalgic look and feel of Facebook's old-school design. </span>
+                    <span>I built this to show off frontend and backend development skills, including Node.js, Express, React, and more! </span>
+                </div>
+            </div>
         </div>
         </>
     )
